ed5c28ec1ce11c0a6f090e56c33b6f25dd9ccfe5
Matt Howell <misterinterrupt@gmail.com>
2014-07-28 18:21:26 -0700

    well that was ridiculous..

--- mcp_provision.sh
+++ mcp_provision.sh
@@ -1,5 +1,5 @@
 
-#!/bin/bash
+#!/bin/
 export DEBIAN_FRONTEND=noninteractive
 
 cd ~/
@@ -7,16 +7,19 @@ ln -s /var/lib/cloud9/ bone
 mkdir
 mkdir downloads
 ## get the config repo if we can and do some simple stuff first
-HAS_CONFIG_REPO=git clone git@github.com:misterinterrupt/bone-configure.git
 
-if $HAS_CONFIG_REPO ; then
-	cd bone-configure
-	cp configs/mcp/hostname /etc/hostname
-	cp configs/mcp/interfaces /etc/network/interfaces
-	ntpdate -b -s -u pool.ntp.org
+if [ls bone-configure]; then
+	printf '%s\n' 'can already has config repo.\n' >&2
 else
-	printf '%s\n' 'could not get config repo.\n' >&2
-	exit 1;
+	if git clone git@github.com:misterinterrupt/bone-configure.git; then
+		cd bone-configure
+		cp configs/mcp/hostname /etc/hostname
+		cp configs/mcp/interfaces /etc/network/interfaces
+		ntpdate -b -s -u pool.ntp.org
+	else
+		printf '%s\n' 'could not get config repo.\n' >&2
+		exit 1;
+	fi
 fi
 
 ## get the package manager working as well as we can
@@ -25,24 +28,30 @@ apt-get -y install
 apt-get -y couchdb
 cd ~/bone/javascripts
 
-HAS_MCP_REPO=git clone https://github.com/misterinterrupt/mixpanel-ddc-master
-
-if $HAS_MCP_REPO ; then
-	cd mixpanel-ddc-master
-else
-	printf '%s\n' 'could not get mcp repo.\n' >&2
-	exit 1
+if [ls mixpanel-ddc-master]; then
+	printf '%s\n' 'can already has mcp repo.\n' >&2	
+else 
+	if git clone https://github.com/misterinterrupt/mixpanel-ddc-master; then
+		cd mixpanel-ddc-master
+	else
+		printf '%s\n' 'could not get mcp repo.\n' >&2
+		exit 1
+	fi
 fi
 
-if [!npm install] ; then
+if [npm install] ; then
+	:
+else
 	printf '%s\n' 'could not install npm dependencies.\n' >&2
 	exit 1
 fi
 
-BOWER_INSTALLED=bower install
-if [!BOWER_INSTALLED] ; then
-		printf '%s\n' 'could not install npm dependencies.\n' >&2
-		exit 1
+if bower install ; then
+	:
+else	
+	printf '%s\n' 'could not install npm dependencies.\n' >&2
+	exit 1
 fi
 
 printf '%s\n' 'dependencies are all installation.\n' >&2
+
---
+++ remote_provision_mcp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## leaving it up to you to make sure you dont run this outside of this fuckin directory
+
+ssh root@
